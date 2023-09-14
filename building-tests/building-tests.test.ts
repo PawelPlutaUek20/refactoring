c091f4d4593e72d1cce693f26da203a4eebcb62b
PawelPlutaUek20 <pawelpluta58@gmail.com>
2023-09-14 21:21:21 +0200

    Add another test

--- building-tests/building-tests.test.ts
+++ building-tests/building-tests.test.ts
@@ -1,8 +1,64 @@
 import { Province, sampleProvinceData } from "./building-tests";
 
-describe("province", function() {
-  it("shortfall", function() {
-    const asia = new Province(sampleProvinceData());
+describe("province", function () {
+  let asia: Province;
+
+  beforeEach(function () {
+    asia = new Province(sampleProvinceData());
+  });
+
+  it("shortfall", function () {
     expect(asia.shortfall).toBe(5);
+  });
+
+  it("profit", function () {
+    expect(asia.profit).toBe(230);
+  });
+
+  it("change production", function () {
+    asia.producers[0].production = 20;
+    expect(asia.shortfall).toBe(-6);
+    expect(asia.profit).toBe(292);
+  });
+
+  it("zero demand", function () {
+    asia.demand = "0";
+    expect(asia.shortfall).toBe(-25);
+    expect(asia.profit).toBe(0);
+  });
+
+  it("negative demand", function () {
+    asia.demand = "-1";
+    expect(asia.shortfall).toBe(-26);
+    expect(asia.profit).toBe(-10);
+  });
+
+  it("empty string demand", function () {
+    asia.demand = "";
+    expect(asia.shortfall).toBeNaN();
+    expect(asia.profit).toBeNaN();
+  });
+});
+
+describe("no producers", function () {
+  let noProducers: Province;
+
+  beforeEach(function () {
+    const data = {
+      name: "No producers",
+      producers: [],
+      demand: 30,
+      price: 20,
+    };
+
+    noProducers = new Province(data);
+  });
+
+  it("shortfall", function () {
+    expect(noProducers.shortfall).toBe(30);
+  });
+
+  it("profit", function () {
+    expect(noProducers.profit).toBe(0);
   });
 });
